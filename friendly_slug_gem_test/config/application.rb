bcf0d3a71ff18af191545a4afe7a0b4bfe203ea0
Sam Holst <sawohol@gmail.com>
2018-06-28 23:09:26 -0600

    removed gem and place file in config app to load for testing

--- friendly_slug_gem_test/config/application.rb
+++ friendly_slug_gem_test/config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative "../../lib/friendly_slug/active_record/base"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
