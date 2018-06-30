060232ff37f7a3535c7767289cc3f581fc10f31f
Sam Holst <sawohol@gmail.com>
2018-06-29 23:29:35 -0600

    renamed variables

--- lib/friendly_slug/active_record/base.rb
+++ lib/friendly_slug/active_record/base.rb
@@ -2,32 +2,32 @@ module FriendlySlug
   module ActiveRecord
     module Base
       def build_friendly_slug(*attribute_list, use: nil)
-        instance_variable_set("@_attribute_list", attribute_list)
-        instance_variable_set("@_use_key", use)
+        instance_variable_set("@_friendly_attribute_list", attribute_list)
+        instance_variable_set("@_friendly_use_key", use)
 
         instance_eval do
-          def _attribute_list
-            @_attribute_list
+          def _friendly_attribute_list
+            @_friendly_attribute_list
           end
 
-          _attribute_list.each do |attribute|
+          _friendly_attribute_list.each do |attribute|
             define_singleton_method :"_friendly_#{attribute.to_s}_key" do
               instance_variable_set("@_friendly_#{attribute.to_s}_key", attribute)
             end
           end
 
-          def _use_key
-            @_use_key
+          def _friendly_use_key
+            @_friendly_use_key
           end
 
           def find_slugged(id)
-            find(id.split("-").send(_use_key))
+            find(id.split("-").send(_friendly_use_key))
           end
         end
 
         class_eval do
           def to_param
-            self.class._attribute_list.map do |attribute|
+            self.class._friendly_attribute_list.map do |attribute|
               lookup_key(self.class.send("_friendly_#{attribute.to_s}_key")).to_s
             end.join("-").gsub(/<\/?[^>]*>|[^\w\s-]/, '').strip.downcase.gsub(/\s{1,}/, '-')
           end
