9ae2cf955d1a3caf6600d284a96a6ec4ca43c242
Sam Holst <sawohol@gmail.com>
2018-06-30 11:07:43 -0600

    abstracted out private methods

--- lib/friendly_slug/active_record/base.rb
+++ lib/friendly_slug/active_record/base.rb
@@ -30,7 +30,7 @@ module FriendlySlug
 
           def to_param
             if self.respond_to?(:slug)
-              if self.slug.nil? || self.send("#{self.class.send("_friendly_attribute_list").first.to_s}_changed?".to_sym)
+              if self.slug.nil? || _unique_attribute_changed?
                 _create_slug
               else
                 self.slug
@@ -48,7 +48,7 @@ module FriendlySlug
           def _update_slug
             if self.class._friendly_use_key == :database
               current_slug = self.to_param
-              unless self.class.where("slug = ? AND id != ?", current_slug, self.id.nil? ? "NULL" : self.id).any? 
+              unless _slug_exists?(current_slug)
                 self.slug = current_slug
               else
                 self.slug = [current_slug.to_s, SecureRandom.hex(6)].join("-")
@@ -60,6 +60,14 @@ module FriendlySlug
             self.class._friendly_attribute_list.map do |attribute|
               _lookup_key(self.class.send("_friendly_#{attribute.to_s}_key")).to_s
             end.join("-").gsub(/<\/?[^>]*>|[^\w\s-]/, '').strip.downcase.gsub(/\s{1,}/, '-')
+          end
+
+          def _unique_attribute_changed?
+            self.send("#{self.class.send("_friendly_attribute_list").first.to_s}_changed?".to_sym)
+          end
+
+          def _slug_exists?(current_slug)
+            self.class.where("slug = ? AND id != ?", current_slug, self.id.nil? ? "NULL" : self.id).any? 
           end
         end
       end
