b68f4a3c1318ad1dfb7609b0a668454d8c1273e1
Sam Holst <sawohol@gmail.com>
2018-06-30 00:08:16 -0600

    added topics to seeds

--- friendly_slug_gem_test/db/seeds.rb
+++ friendly_slug_gem_test/db/seeds.rb
@@ -3,3 +3,7 @@ titles = ["Wazzzup People", "Howdy all me peeps!", "this is the time to rock $15
 3.times do |i|
   Blog.create!(title: titles[i], body: "Just blank text.")
 end
+
+3.times do |i|
+  Topic.create!(title: titles[i])
+end
